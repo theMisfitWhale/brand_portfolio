dcb2a8faa614b7d5b6091e665c66dc90f25887f9
sudhir <sudhir9297@gmail.com>
2023-02-23 11:32:32 +0530

    feat: added prettier-plugin-tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           <br className="hidden lg:block" />
-          <span className="inline-block bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent p-12">
+          <span className="inline-block bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text p-12 text-transparent">
             Next.js + tailwindcss
           </span>{' '}
         </h1>
