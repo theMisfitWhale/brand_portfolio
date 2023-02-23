4c9503f92ba29b9e7d6b697efa8875f155f82948
sudhir <sudhir9297@gmail.com>
2023-02-23 19:54:28 +0530

    fix: background color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import { Button } from '../components';
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -9,12 +7,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <span className="from-red-500 inline-block  bg-gradient-to-r to-blue-500 bg-clip-text p-12 text-6xl font-extrabold leading-[1.1] tracking-tighter  sm:text-7xl lg:text-8xl xl:text-8xl">
+        <span className="inline-block p-12 text-6xl font-extrabold leading-[1.1] tracking-tighter sm:text-7xl lg:text-8xl xl:text-8xl">
           Next.js + tailwindcss
-        </span>{' '}
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
-          <Button />
-        </div>
+        </span>
       </main>
     </div>
   );
