294dc46327ff80fda04d00bcf61e0a62f3479e4f
sudhir <sudhir9297@gmail.com>
2023-02-23 12:22:53 +0530

    feat: loading font

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          <br className="hidden lg:block" />
-          <span className="inline-block bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text p-12 text-transparent">
-            Next.js + tailwindcss
-          </span>{' '}
-        </h1>
+        <span className="from-red-500 inline-block  bg-gradient-to-r to-blue-500 bg-clip-text p-12 text-6xl font-extrabold leading-[1.1] tracking-tighter  sm:text-7xl lg:text-8xl xl:text-8xl">
+          Next.js + tailwindcss
+        </span>{' '}
         <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
           <Button />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#ed7c50',
+        darkOrange: '#d96d43',
+        darkGrey: '#595959',
+        lightGrey: '#8a8a8a',
+        green: '#43ae61',
+        purple: '#8566f6',
+        turks: '#a6e2e3',
+        red: '#e46060',
+        black: '#282829',
+        white: '#f5f4f5',
+      },
+    },
   },
   plugins: [],
 };
