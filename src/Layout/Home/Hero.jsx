03e6f6db4c7ba34ac8de83b77d5f5c3bdff667c8
sudhir <sudhir9297@gmail.com>
2023-03-09 14:54:41 +0530

    Text reveal header

---
+++ src/Layout/Home/Hero.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect } from 'react';
+import { FlowerLogo } from '../../components/Icons';
+import gsap from 'gsap';
+import { CustomEase } from 'gsap/dist/CustomEase';
+
+gsap.registerPlugin(CustomEase);
+CustomEase.create('cubic-text', '0.25, 1, 0.5, 1');
+
+export const HomeHero = () => {
+  useEffect(() => {
+    const titles = document.querySelectorAll('.h_title');
+    const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+    titles.forEach((title, index) => {
+      const el = title.querySelectorAll('span span');
+      const delay = index * 0.08;
+
+      tl.to(
+        el,
+        {
+          y: 0,
+          duration: 1.5,
+          ease: 'cubic-text',
+        },
+        delay
+      );
+    });
+
+    return () => {};
+  }, []);
+
+  return (
+    <div className=" min-h-screen pt-[120px] pb-[90px] lg:py-[180px]">
+      <div className="h_container font_apoc pb-[calc(5.8vw + 60px)] md:pb-[calc(9.8vw + 60px)] flex flex-col items-center px-2 lg:pb-[4vw]">
+        <h1 className="h_title relative w-full text-center text-[8.7vw] font-light uppercase leading-[90%] ">
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className=" inline-block  translate-y-full pb-1.5 pt-6 will-change-transform ">
+              You&nbsp;
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform [&>svg]:w-[11vw] lg:[&>svg]:w-[6.9vw]">
+              <FlowerLogo />
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              &nbsp;Can
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              &nbsp;Only
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              Build&nbsp;
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              Brands&nbsp;
+            </span>
+          </span>
+        </h1>
+        <h1 className="h_title relative w-full text-center text-[8.7vw] font-light uppercase leading-[90%] ">
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className=" inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              People&nbsp;
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              Love
+            </span>
+          </span>
+        </h1>
+        <h1 className="h_title relative w-full text-center text-[8.7vw] font-light uppercase leading-[90%] ">
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="font_aeonik inline-block  translate-y-full pb-1.5  pt-6 text-[8.7vw]  font-medium leading-[81%] text-orange will-change-transform ">
+              By Loving
+            </span>
+          </span>
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              &nbsp;Your
+            </span>
+          </span>
+        </h1>
+        <h1 className="h_title relative w-full text-center text-[8.7vw] font-light uppercase leading-[90%] ">
+          <span className="-mb-1.5 -mt-6 inline-block overflow-hidden align-bottom">
+            <span className="inline-block  translate-y-full pb-1.5 pt-6 will-change-transform">
+              Audience
+            </span>
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Header } from '@/components';
+import { HomeHero } from '../Layout';
 
 export default function Home() {
   return (
@@ -10,25 +11,8 @@ export default function Home() {
       </Head>
       <main className=" w-auto ">
         <Header />
-        <div className=" block px-20 pt-40  text-[7vh] ">
-          <div>Athens, Greece</div>
-          <div>Jaipur, Rajasthan, India</div>
-          <div>Bergen, Norway</div>
-          <div>Marrakesh, Morocco</div>
-          <div>Dublin, Ireland</div>
-          <div>Cartagena, Colombia</div>
-          <div>Bangkok, Thailand</div>
-          <div>San Sebastian, Spain</div>
-          <div>Dubrovnik, Croatia</div>
-          <div>Seoul, South Korea</div>
-          <div>San Miguel de Allende, Mexico</div>
-          <div>Queenstown, New Zealand</div>
-          <div>Hanoi, Vietnam</div> <div>Bangkok, Thailand</div>
-          <div>San Sebastian, Spain</div>
-          <div>Dubrovnik, Croatia</div>
-          <div>Seoul, South Korea</div>
-          <div>San Miguel de Allende, Mexico</div>
-          <div>Queenstown, New Zealand</div>
+        <div className="bg-white">
+          <HomeHero />
         </div>
       </main>
     </div>
